5ed45a7cc902d3d7d8cdd2395b8f3323dfb01849
Evgeny <e.g.belyansky@gmail.com>
2026-07-05 12:55:54 +0400

    Initial commit

---
+++ src/main/java/ru/pet_project/order_app/OrderAppApplication.java
@@ -0,0 +1,13 @@
+package ru.pet_project.order_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pet_project/order_app/OrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet_project.order_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
